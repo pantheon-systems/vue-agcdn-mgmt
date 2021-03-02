ccde59893b612a8dd742f9a61d6aa338daf2340b
Tom Mount <tmountjr@gmail.com>
2021-03-02 15:31:08 -0500

    remove testing api url

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App.vue'
 import axios from 'axios';
 Vue.prototype.$api = axios.create({
   baseURL: 'https://us-central1-pantheon-psapps.cloudfunctions.net/advcdn/client_management',
-  // baseURL: 'https://8911e4411299.ngrok.io/client_management',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
